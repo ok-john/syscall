82d3c7aefae61f3742a009981c9af80617cbcaf1
John Sahhar <john@entropy.xyz>
2023-01-30 11:10:52 -0500

    add support for arm64e syscalls

---
+++ src/mod.rs
@@ -0,0 +1,8 @@
+// #[cfg(feature="linux")]
+// pub(crate) mod syscall_darwin;
+// #[cfg(feature="linux")]
+// pub(crate) mod syscalls;
+// #[cfg(feature="darwin")]
+// pub(crate) mod syscall_linux;
+
+
